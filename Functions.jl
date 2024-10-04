be4ccdde25e13d928e7021a3a28935165c66aab1
NicolauUP <up201805451@fc.up.pt>
2024-10-04 11:26:17 +0100

    Upload The first Files

---
+++ Functions.jl
@@ -0,0 +1,127 @@
+
+
+using FFTW
+
+function moire_vectors(a1,a2,m,r)
+    if gcd(r,3) == 1
+        t1 = m .* a1 .+ (m+r) .* a2
+        t2 = -(m+r) .* a1 .+ (2*m+r) .* a2
+    else
+        t1 = (m+r/3) .* a1 .+ r/3 .* a2
+        t2 = -r/3 .* a1 + (m + 2*r/3) .* a2
+    end
+    return t1,t2
+end
+
+function moire_angle(m,r)
+    return acos((3*m^2 + 3*m*r + 0.5*r^2)/(3*m^2 + 3*m*r + r^2))
+end
+
+function rotationMatrix(θ)
+    return SA[cos(θ) -sin(θ); sin(θ) cos(θ)]
+end
+
+function t_perp_dist(dr, d_perp , d, δ , t , t_perp )
+    term1 = (d_perp^2 / (norm(dr)^2 + d_perp^2)) * t_perp * exp((d_perp - sqrt(norm(dr)^2+d_perp^2)) / δ)
+    term2 = norm(dr)^2 / (norm(dr)^2 + d_perp^2) * (-t) * exp((d - sqrt(norm(dr)^2+d_perp^2)) / δ)
+    return term1 + term2
+end
+
+
+function ChargeRatio(Es, Vecs, r_max,sites1, sites2)
+    Result = zeros(Float64, length(Es))
+
+    Condition1 = (norm.(sites1) .<= r_max)
+    Condition2 = (norm.(sites2) .<= r_max)
+    Condition = [Condition1; Condition2]
+    Fraction_Of_Sites = sum(Condition) / length(Condition)
+
+    for i in 1:length(Es)
+        Result[i] = real(sum(abs2.(Vecs[1:end, i]) .* Condition) / Fraction_Of_Sites)
+    end
+    return Result
+end
+
+function LDOS_Bins(Energies, EigenValues, Vectors, R_Max,sites)
+  Results =  zeros(Float64,length(Energies))
+    for i in eachindex(EigenValues)
+        if Energies[1] <= EigenValues[i] <= Energies[end]
+            indice = floor(length(Energies).*(EigenValues[i] .- Energies[1]) ./(Energies[end] - Energies[1]) )
+            for r in eachindex(sites)
+                if norm(sites[r]) <= R_Max
+                    Results[i] += abs2.(Vectors[r,i])
+                    
+                end
+            end
+        end
+    end
+    return Results ./ (length(sites) * (Energies[2] - Energies[1])) #Normalized by bin and number of sites
+end
+
+function IPR(Es,Vecs,sites,R_Max)
+    result = zeros(Float64, length(Es))
+    Condition = (norm.(sites) .<= R_Max)
+    for i in eachindex(Es)
+        result[i] = sum(abs2.(Vecs[1:end,i]).^2 .* Condition) / (sum(abs2.(Vecs[1:end,i]) .* Condition)).^2
+    end
+    return 
+end
+
+function find_first_OrbB(sites1,d_cc)
+    N = length(sites1)
+    N_teste = Int64(floor(N/2))
+    for i in N_teste-100:N_teste+100
+        dist_firstA = norm(sites1[1] .- sites1[i])
+        if dist_firstA <= 5*d_cc
+            NB = i
+            return NB
+            break
+        end
+    end
+    
+end
+
+
+
+
+function Sort_Vecs_and_FFT(nev, L_F ,sites1, δ_F, Vecs, inverse,NB)
+
+    Vectors2D_Rhombus_OrbA = zeros(Complex{Float64}, nev, Int64(L_F),Int64(L_F))
+    Vectors2D_Rhombus_OrbB = zeros(Complex{Float64}, nev, Int64(L_F),Int64(L_F))
+
+    for i in eachindex(sites1)
+        Index = Int64.(floor.(inverse * (sites1[i] .- δ_F)))
+        # println(Index[1] , " ",Index[2]," " , L_F)
+        if 0 <= Index[1] < L_F && 0 <= Index[2] < L_F
+            if i <= NB
+                
+                Vectors2D_Rhombus_OrbA[1:end, Index[1]+1,Index[2]+1] = Vecs[i,1:end]
+            else    
+                
+                Vectors2D_Rhombus_OrbB[1:end, Index[1]+1,Index[2]+1] = Vecs[i,1:end]
+            end
+        end
+
+    end 
+    
+    Psi_KA = zeros(Complex{Float64}, nev, L_F,L_F)
+    Psi_KB = zeros(Complex{Float64}, nev, L_F,L_F)
+
+    for i in 1:nev
+        
+        Psi_KA[i,1:end,1:end]= abs.(fft(Vectors2D_Rhombus_OrbA[i,1:end,1:end]))
+        Psi_KB[i,1:end,1:end]= abs.(fft(Vectors2D_Rhombus_OrbB[i,1:end,1:end]))
+    
+    end
+
+    return Psi_KA, Psi_KB
+end
+
+function Compute_IPRK(Psi_KA, Psi_KB, EigenValues)
+
+    resultado = zeros(Float64, length(EigenValues)) 
+    for i in eachindex(resultado)
+        resultado[i] = sum(abs2.(Psi_KA[i,1:end,1:end]).^2 +abs2.(Psi_KB[i,1:end,1:end]).^2 ) /(sum(abs2.(Psi_KA[i,1:end,1:end]) + abs2.(Psi_KB[i,1:end,1:end]) )).^2
+    end
+return resultado
+end
---
+++ Run_and_Eigs.jl
@@ -0,0 +1,152 @@
+
+using StaticArrays 
+using Quantica
+using Arpack
+using SparseArrays #Necessário para poder ler sparse matrices do jld
+using JLD2 #NPZ do Julia
+using FileIO
+using LinearAlgebra
+
+include("Functions.jl")
+
+
+
+#INPUTS
+
+# m = parse(Int64, ARGS[1]) #tBLG approximant
+# r = parse(Int64, ARGS[2]) #tBLG approximant 
+# P_QP = parse(Int64, ARGS[3]) #Periodic or quasiperiodic
+# nev = parse(Int64, ARGS[4]) #Number of states in Arpack
+
+m = 10
+r = 1
+P_QP = 1
+nev = 10
+
+#Constantes da geometria
+a0 = 2.46 
+d_perp = 3.35 
+d_cc = 1.42 
+Λ = 2.5*d_cc
+δ = 0.184*a0
+
+#Hamiltonian parameters
+t = 2.7 #eV
+t_perp = 1.9  #eV
+#Honeycomb lattice vectors
+a1 = a0 .* SA[1/2,sqrt(3)/2] #Static array de float64 
+a2 = a0 .* SA[-1/2, sqrt(3)/2] #Static Array de Float64
+
+#tBLG periodic approximants
+
+
+t1,  t2 = moire_vectors(a1,a2,m,r)
+L_supercell = norm(t1)
+
+θ = moire_angle(m,r)
+if P_QP == 1
+    R = L_supercell * sqrt(3) / 4 #Biggest disk inside unit cell
+else
+    t1_P, t2_P = moire_vectors(a1,a2,30,1) #Best approximant to the magic angle.
+    L_moire = normm(t1_P)
+    R = round(L_supercell / L_moire) * sqrt(3) / 4
+end
+
+#Quantica SETUP
+
+OrbA1 = sublat((0.0,0.0),name=:A1)
+OrbB1 = sublat((1/3 * (a1[1]+a2[1]),1/3 * (a1[2]+a2[2])),name=:B1)   
+
+δ_stacking = -1/3 .* (a1 .+ a2) #AB Stacking # + Shift Vector .
+OrbA2 = sublat((δ_stacking[1],δ_stacking[2]),name=:A2)
+OrbB2 = sublat((δ_stacking[1]+1/3 * (a1[1]+a2[1]),δ_stacking[2] + 1/3 *(a1[2]+a2[2])),name=:B2)
+
+Layer1_UC = lattice(OrbA1, OrbB1, bravais = (a1, a2),dim=2)
+Layer2_UC = lattice(OrbA2, OrbB2, bravais = (a1, a2),dim=2)
+Layer2_UC = transform(Layer2_UC, r -> rotationMatrix(θ) * r)
+
+Layer1 = supercell(Layer1_UC, region = r -> 0 <= norm(r) <= R)
+Layer2 = supercell(Layer2_UC, region = r -> 0 <= norm(r) <= R)
+
+model_graphene = hopping(-t)
+
+h11 = hamiltonian(Layer1, model_graphene)
+h22 = hamiltonian(Layer2, model_graphene)
+
+
+model_inter12 = hopping((r,dr) -> t_perp_dist(dr, d_perp , d_cc, δ , t , t_perp ),range = Λ)
+
+
+H = combine(h11, h22, coupling = model_inter12)
+
+
+
+#Obtain Eigenvalues and EigenVectors
+
+println(" ******* EigenDecomposition Started ******")
+
+σ_ARPACK = 0.0135 #Center of the FlatBand
+Vals, Vecs = eigs(H(()), nev=nev, maxiter=1000, tol=1e-4, sigma=σ_ARPACK)
+Es = real(Vals)
+
+
+#Sort Eigenvalues and Eigenvectors 
+
+sorted_indices = sortperm(Es)
+Es = Es[sorted_indices]
+Vecs = Vecs[1:end, sorted_indices]
+
+
+println(" ******* EigenDecomposition Finised ******")
+
+#Charge distribution
+
+sites1 = sites(Layer1)
+sites2 = sites(Layer2)
+rs = LinRange(R/20, R,20)
+Resultado_Carga = Vector{Vector{Float64}}()
+for i in eachindex(rs)
+    push!(Resultado_Carga,ChargeRatio(Es, Vecs, rs[i],sites1, sites2))
+end
+
+# #Density of states
+sites_Com = [sites1; sites2] 
+
+Energies_DOS = LinRange(0.004*2.7,0.006*2.7, Int64(round((0.002*2.7) / 0.0001)))
+DOS_Total = LDOS_Bins(Energies_DOS, Es, Vecs, R,sites_Com)
+DOS_Bulk = LDOS_Bins(Energies_DOS, Es, Vecs, 0.8*R,sites_Com)
+
+    
+#IPR
+IPR_Bulk = IPR(Es,Vecs,sites_Com,0.8*R)
+IPR_Total = IPR(Es,Vecs,sites_Com,R)
+
+
+
+
+#Calcular Rhombus e FFT
+L_F = Int64(round(sqrt(length(sites1)/2)))
+InvA = [a1[1] a2[1]; a2[2] a2[2]] ^-1
+δ_F = -0.5 * L_F .* (a1 .+ a2)
+
+#Implement Find Index of First B 
+indexB1 = find_first_OrbB(sites1,d_cc)
+Psi_KA, Psi_KB = Sort_Vecs_and_FFT(nev, L_F, sites1,δ_F, Vecs,InvA,indexB1) # Falta calcular delta, inverse e L_rhombus
+
+
+
+#Calculate IPR_K,
+IPR_K = Compute_IPRK(Psi_KA, Psi_KB, Es)
+
+
+save("TBG_Results_m=$(m)_r=$(r)_PQP=$(P_QP)_nev=$(nev)_σA=$(σ_ARPACK).jld2",
+    "EigenValues",Es,
+    "Rs_Charges",rs,
+    "Charge",Resultado_Carga,
+    "EnergiesDOS",Energies_DOS,
+    "DOS_Total",DOS_Total,
+    "DOS_Bulk",DOS_Bulk,
+    "IPR_Bulk",IPR_Bulk,
+    "IPR_Total",IPR_Total,
+    "L_F",L_F,
+    "IPR_K",IPR_K)
